0cc295d5c454bf469cb2b34148a0860a300aa6ed
mAAdhaTTah <jamesorodig@gmail.com>
2018-06-15 11:13:06 -0400

    Add test for multiple languages
    This appeared to be an issue but wasn't, but keeping the test
    anyway.

---
+++ test/fixtures/several-languages/expected.js
@@ -0,0 +1,6 @@
+import Prism from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-reason";
