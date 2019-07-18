9a0adae92cc4023e8fa07d617b956eb89c286469
mAAdhaTTah <jamesorodig@gmail.com>
2019-07-18 13:05:12 -0400

    Add getName to search aliases
    This adds support for aliases.
    
    Fixes #4.

--- src/getComponents.js
+++ src/getComponents.js
@@ -3,6 +3,33 @@ import config from 'prismjs/components.js';
 const getPath = type => name =>
     `prismjs/${config[type].meta.path.replace(/\{id\}/g, name)}`;
 
+const getName = (type, name) => {
+    if (type !== 'languages') {
+        return name;
+    }
+
+    if (typeof config[type][name] !== 'undefined') {
+        return name;
+    }
+
+    for (const key in config.languages) {
+        if (key === 'meta') {
+            continue;
+        }
+
+        const { alias } = config.languages[key];
+
+        if (typeof alias === 'string' && alias === name) {
+            return key;
+        }
+
+        if (Array.isArray(alias) && alias.includes(name)) {
+            return key;
+        }
+    }
+
+    throw new Error(`Language ${name} is invalid. Is this a typo?`);
+};
 const getRequire = (type, name) => config[type][name].require;
 const getPeers = (type, name) => config[type][name].peerDependencies;
 const getNoCSS = (type, name) => config[type][name].noCSS;
@@ -27,6 +54,7 @@ const getDependencies = (type) => function getDependencies(deps, newDeps) {
     }
 
     deps = newDeps.reduce((deps, dep) => {
+        dep = getName(type, dep);
         deps = getDependencies(deps, getRequire(type, dep));
 
         if (!deps.includes(dep)) {
