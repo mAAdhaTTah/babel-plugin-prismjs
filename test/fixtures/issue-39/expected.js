d4c3a06a44f63877f16c7b4fb9087fa9f2699b0b
James DiGioia <jamesorodig@gmail.com>
2020-01-19 17:37:05 -0500

    Update path based on type loaded (#68)
    Use the loader for one single load, rather than two separate
    loads.
    
    Fixes #39.

---
+++ test/fixtures/issue-39/expected.js
@@ -0,0 +1,10 @@
+import Prism from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers";
+import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
+import "prismjs/components/prism-diff";
+import "prismjs/plugins/diff-highlight/prism-diff-highlight.css";
+import "prismjs/plugins/diff-highlight/prism-diff-highlight";
+import "prismjs/themes/prism.css";
