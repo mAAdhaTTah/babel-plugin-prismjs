e973aee5cdfb1dc0bdec1b8cde4f3fe25eeb04d2
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-07-02 15:40:25 +0200

    Added support for loading all languages (#110)

--- src/getComponents.js
+++ src/getComponents.js
@@ -1,12 +1,31 @@
+/* eslint-disable valid-jsdoc */
 import config from 'prismjs/components.js';
 import getLoader from 'prismjs/dependencies.js';
 
+/**
+ * @param {string} type
+ * @returns {(name: string) => string}
+ */
 const getPath = type => name =>
     `prismjs/${config[type].meta.path.replace(/\{id\}/g, name)}`;
 
+/**
+ * @param {string} dep
+ * @returns {boolean}
+ */
 const isPlugin = dep => config.plugins[dep] != null;
-const getNoCSS = (type, name) => config[type][name].noCSS;
 
+/**
+ * @param {string} type
+ * @param {string} name
+ * @returns {boolean}
+ */
+const getNoCSS = (type, name) => !!config[type][name].noCSS;
+
+/**
+ * @param {string} theme
+ * @returns {string}
+ */
 const getThemePath = theme => {
     if (theme === 'default') {
         theme = 'prism';
@@ -20,16 +39,32 @@ const getThemePath = theme => {
 const getPluginPath = getPath('plugins');
 const getLanguagePath = getPath('languages');
 
-export default ({ languages = [], plugins = [], theme, css = false } = {}) => [
-    ...getLoader(config, [...languages, ...plugins]).getIds().reduce((deps, dep) => {
-        // Plugins can have language dependencies.
-        const add = [isPlugin(dep) ? getPluginPath(dep) : getLanguagePath(dep)];
+/**
+ * @param {Options} [options]
+ * @returns {string[]}
+ *
+ * @typedef Options
+ * @property {string[] | 'all'} [languages]
+ * @property {string[]} [plugins]
+ * @property {string} [theme]
+ * @property {boolean} [css]
+ */
+export default ({ languages = [], plugins = [], theme, css = false } = {}) => {
+    if (languages === 'all') {
+        languages = Object.keys(config.languages).filter(l => l !== 'meta');
+    }
 
-        if (css && isPlugin(dep) && !getNoCSS('plugins', dep)) {
-            add.unshift(getPluginPath(dep) + '.css');
-        }
+    return [
+        ...getLoader(config, [...languages, ...plugins]).getIds().reduce((deps, dep) => {
+            // Plugins can have language dependencies.
+            const add = [isPlugin(dep) ? getPluginPath(dep) : getLanguagePath(dep)];
 
-        return [...deps, ...add];
-    }, []),
-    ...(css && theme ? [getThemePath(theme)] : [])
-];
+            if (css && isPlugin(dep) && !getNoCSS('plugins', dep)) {
+                add.unshift(getPluginPath(dep) + '.css');
+            }
+
+            return [...deps, ...add];
+        }, /** @type {string[]} */([])),
+        ...(css && theme ? [getThemePath(theme)] : [])
+    ];
+};
